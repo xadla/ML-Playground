265ce8724478483f4bf1f62b9c4319d8daa91d81
xadla <hadinjr4122@gmail.com>
2025-04-30 09:39:15 +0330

    Add Custom User to models

--- backend/ml_playground/settings.py
+++ backend/ml_playground/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # my apps.
+    "accounts.apps.AccountsConfig",
 ]
 
 MIDDLEWARE = [
@@ -73,9 +76,13 @@ WSGI_APPLICATION = 'ml_playground.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+    "ENGINE": "django.db.backends.postgresql",
+    "NAME": "ml_pg",
+    "USER": "ml",
+    "PASSWORD": "ml",
+    "HOST": "localhost",
+    "PORT": "5432",
     }
 }
 
@@ -120,3 +127,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+AUTH_USER_MODEL = "accounts.User"
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,4 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from "react";
+import { createRoot } from "react-dom/client";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+
