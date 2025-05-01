ec67e03b9562e2aaa324c491ce5761995fb27110
xadla <hadinjr4122@gmail.com>
2025-05-01 07:53:07 +0330

    initial commit for frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div className="p-8 bg-blue-100 text-center">
+      <h1 className="text-3xl font-bold text-blue-900">Hello Tailwind + Vite + React!</h1>
+    </div>
+  );
+}
+
+export default App;
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,4 +1,12 @@
 import React from "react";
-import { createRoot } from "react-dom/client";
+import ReactDOM from "react-dom/client";
 
 
+import App from "./App"
+
+// sytles
+import './index.css';
+
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
