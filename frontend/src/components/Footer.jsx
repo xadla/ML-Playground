6864672409d943a52164b3aa1978a95677f33280
xadla <hadinjr4122@gmail.com>
2025-05-01 19:07:58 +0330

    Add footer

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -3,14 +3,14 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <footer className="bg-gray-800 text-white py-4">
+    <footer className="bg-white text-gray-900 py-4">
       <div className="container mx-auto text-center">
         <p>&copy; 2023 Your Company. All rights reserved.</p>
         <p>Follow us on social media:</p>
         <div className="flex justify-center space-x-4 mt-2">
-          <a href="#" className="text-gray-400 hover:text-white">Facebook</a>
-          <a href="#" className="text-gray-400 hover:text-white">Twitter</a>
-          <a href="#" className="text-gray-400 hover:text-white">Instagram</a>
+          <a href="#" className="text-gray-700 hover:text-indigo-600">Facebook</a>
+          <a href="#" className="text-gray-700 hover:text-indigo-600">Twitter</a>
+          <a href="#" className="text-gray-700 hover:text-indigo-600">Instagram</a>
         </div>
       </div>
     </footer>
