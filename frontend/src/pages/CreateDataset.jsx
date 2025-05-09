ca5ecfb0bee52099347f4cfdf361430a315783c5
xadla <hadinjr4122@gmail.com>
2025-05-09 10:37:20 +0330

    Update Upload section

--- frontend/src/pages/CreateDataset.jsx
+++ frontend/src/pages/CreateDataset.jsx
@@ -1,15 +1,15 @@
 import React from "react";
 
-import UploadDataset from "../components/dataset/upload";
-import CanvasComponent from "../components/dataset/canvas";
+import UploadDataset from "../components/dataset/UploadDataset";
+import DatasetCreator from "../components/dataset/DatasetCreator";
 
 const CreateDataset = () => {
 
   return (
     <div className="p-4">
-      <h2 className="text-xl font-bold mb-4">Create or Upload Dataset</h2>
+      <h2 className="font-bold mb-4 text-center text-4xl">Create or Upload Dataset</h2>
+      <DatasetCreator />
       <UploadDataset />
-      <CanvasComponent />
     </div>
   );
 };
